fdc24e573b212df7adddc63dfe695f6309eabc8a
Carl Mai <carl.schoenbach@gmail.com>
2023-10-28 20:36:35 +0200

    feat: check-api use text-formatting and upload an image

--- src/main/java/org/qualityannotate/api/qualitytool/Issue.java
+++ src/main/java/org/qualityannotate/api/qualitytool/Issue.java
@@ -1,11 +1,22 @@
 package org.qualityannotate.api.qualitytool;
 
 import jakarta.annotation.Nullable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
-public record Issue(String fileName, Integer lineNumber, String comment, String severity, String severityIcon,
-        Severity severityEnum, @Nullable String urlToIssue) {
+public record Issue(String fileName, Integer startLine, @Nullable Integer endLine, @Nullable Integer startColumn,
+        @Nullable Integer endColumn, String comment, String severity, Severity severityEnum,
+        @Nullable String urlToIssue) {
 
+    public Integer lineNumber() {
+        return startLine;
+    }
+
+    @AllArgsConstructor
+    @Getter
     public enum Severity {
-        LOW, MEDIUM, HIGH
+        LOW("ℹ\uFE0F"), MEDIUM("⚠\uFE0F"), HIGH("\uD83D\uDED1");
+
+        private final String unicodeIcon;
     }
 }
